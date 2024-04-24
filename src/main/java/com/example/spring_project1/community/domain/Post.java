c889f78d7fd24d49b389fd5ea1f3aeb570f5f78e
narim <ehdrkd648@naver.com>
2024-04-24 18:30:51 +0900

    Design: 댓글페이지, 게시글 페이지, 카테고리 생성 페이지 추가
    - 페이지 레이아웃 및 스타일링 구현

--- src/main/java/com/example/spring_project1/community/domain/Post.java
+++ src/main/java/com/example/spring_project1/community/domain/Post.java
@@ -1,5 +1,13 @@
 package com.example.spring_project1.community.domain;
 
-public class Post {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
+public class Post {
+    @Id
+    //게시글 생성 시 ID 생성을
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 }
